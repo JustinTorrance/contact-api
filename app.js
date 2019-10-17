1bfadddc926a4c09110a4a55966a789f12a5cb02
Justin Torrance <justin.torrance91@gmail.com>
2019-10-17 15:33:39 -0600

    Create endpoints for get, put, delete, and post

--- app.js
+++ app.js
@@ -8,7 +8,56 @@ app.use(express.json());
 app.use(cors())
 
 app.get('/api/v1/contacts', (request, response) => {
-  response.json('howdy')
+  database('contacts').select()
+  .then((contacts) => {
+    response.status(200).json(contacts)
+  })
+  .catch((error) => {
+    response.status(500).json({ error })
+  })
+})
+
+app.delete('/api/v1/contacts/:id', (request, response) => {
+  const idForDelete = request.params.id
+  if(!idForDelete) {
+    response.status(422).json({
+      error: `Missing id from request parameters`
+    })
+  } else {
+    database('contacts')
+      .where('id', request.params.id)
+      .del()
+      .then(() => {
+        response.json(`Successfully deleted contact ${idForDelete}`)
+      })
+      .catch(error => {
+        response.status(500).json({ error })
+      })
+  }
+})
+
+app.post('/api/v1/contacts', (request, response) => {
+  const contact = request.body
+  database('contacts').insert(contact)
+    .then(projects => {
+      response.status(201).json('Contact successfully added')
+    })
+    .catch(error => {
+      response.status(500).json({ error })
+    })
+})
+
+app.put('/api/v1/contacts/:id', (request, response) => {
+  const { first_name, last_name, phone, email } = request.body
+  database('contacts')
+    .where('id', request.params.id)
+    .update({ first_name, last_name, phone, email })
+      .then(contact => {
+        response.status(200).json(`Contact for ${first_name} has been updated`)
+      })
+      .catch(error => {
+        response.status(422).json({ error })
+      })
 })
 
 
